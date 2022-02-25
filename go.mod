381de7df89288223e5ac31a2ffe5ba3c4f476fa1
Ajay Kumar Pottabathula <ajayp@spiceworks.com>
2022-02-25 12:18:32 +0530

    tracker service

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/thinkerajay/tracker-service
+
+go 1.16
+
+require (
+	github.com/gin-gonic/gin v1.7.7
+	github.com/heroku/x v0.0.50
+)
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("templates/*.tmpl.html")
+	router.Static("/static", "static")
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, "success")
+
+	})
+
+	router.Run(":" + port)
+
+}
